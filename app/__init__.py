a053fe4ab152e447bf9d59a706ff04eccb8aecf2
Spiderweak <spiderweak@a-bernard.fr>
2023-11-28 16:09:44 -0500

    Tests real time data reception with SocketIO

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,12 @@ from .routes import setup_routes
 socketio = SocketIO()
 
 def create_app():
+
     app = Flask(__name__)
     socketio.init_app(app)
+
     setup_routes(app)
+
+    from . import socket_routes
+
     return app
--- app/socket_routes.py
+++ app/socket_routes.py
@@ -1,11 +1,44 @@
 from . import socketio
+import whisper
+import io
+import uuid
+import wave
+import logging
+
+
+model = whisper.load_model("base")
+
+# Buffer for accumulating audio data
+audio_buffer = bytearray()
 
 @socketio.on('audio_chunk')
 def handle_audio_chunk(audio_chunk):
-    print('Received audio chunk')
+    global audio_buffer
 
-    # Process the audio_chunk here
-    # For real-time processing, you might buffer these chunks and process periodically
-    # Or if your processing can be done in real-time, handle it directly
+    # Append chunk to the buffer
+    audio_buffer.extend(audio_chunk)
 
-    socketio.emit('transcription', {'text': 'Processed transcription here'})
+    # Check if the buffer has enough data to process (5 seconds of audio)
+    if len(audio_buffer) >= 882000:
+        process_audio_buffer()
+
+
+
+def process_audio_buffer():
+    global audio_buffer
+
+    temp_filename = f"temp_{uuid.uuid4()}.wav"
+
+    with wave.open(temp_filename, 'wb') as temp_file:
+        temp_file.setparams((2, 2, 44100, 0, 'NONE', 'NONE'))
+        temp_file.writeframes(audio_buffer)
+
+    audio_buffer.clear()
+
+    # Process the saved audio file
+    result = model.transcribe(temp_filename)
+    transcription = result["text"]
+
+    # Emit the transcription result
+    socketio.emit('transcription', {'text': transcription})
+    print(transcription)
