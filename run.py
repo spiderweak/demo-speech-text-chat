0a799edfae909971130013a125d0f5e476a2fd63
Spiderweak <spiderweak@a-bernard.fr>
2023-12-18 14:40:16 -0500

    better flask init

--- run.py
+++ run.py
@@ -1,10 +1,18 @@
 from app import create_app, socketio
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = create_app()
 
 if __name__ == '__main__':
     # Passed allow_unsafe_werkzeug=True to disable error : RuntimeError: The Werkzeug web server is not designed to run in production.
     # TODO: Fix this for production
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
-#    app.run(host='0.0.0.0', port=5000, debug=True)
+
+    debug_mode = os.getenv('FLASK_DEBUG', 'False') == 'True'
+    host = os.getenv('FLASK_HOST', '0.0.0.0')
+    port = int(os.getenv('FLASK_PORT', '5000'))
+
+    socketio.run(app, host=host, port=port, debug=debug_mode, allow_unsafe_werkzeug=True)
 
