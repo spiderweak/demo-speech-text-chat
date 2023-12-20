5e63818c71ed375bee24a1fa1153329fcd5e819b
Spiderweak <spiderweak@a-bernard.fr>
2023-12-20 11:00:03 -0500

    Adds comments and typing to __init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,27 @@
+"""Module for initializing the Flask application and its components."""
+
 from flask import Flask
 from flask_socketio import SocketIO
+
 from .routes import setup_routes
 
 socketio = SocketIO(manage_session = True)
 
-def create_app():
+def create_app() -> Flask:
+    """
+    Create and configure an instance of the Flask application.
+
+    Returns:
+        Flask: The created Flask application.
+    """
 
     app = Flask(__name__)
-    socketio.init_app(app)
 
+    socketio.init_app(app)
     setup_routes(app)
 
+    # Importing socket routes here to avoid circular dependencies
     from . import socket_routes
 
     return app
+
